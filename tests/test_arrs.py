62a7051b6f4e5bc4fcea8aab9cdbc052278c146c
Евгений <xarvat@list.ru>
2023-05-03 11:25:41 +0300

    add test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -11,5 +11,6 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([1, 2, 3, 4, 5, 6], 1, 4) == [2, 3, 4]
-    assert arrs.my_slice([1, 2, 3, 4], 2) == [3, 4]
+    assert arrs.my_slice([1, 2, 3], 0) == [1, 2, 3]
+    assert arrs.my_slice([] ) == []
+    assert arrs.my_slice([1, 2, 3], -8) == [1, 2, 3]
